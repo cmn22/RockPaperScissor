5375d8e26e9da69414afb433fb97c21bd550fedf
Chaitanya Malani <chaitanyamalani.rns@gmail.com>
2025-04-01 14:46:01 +0200

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Chaitanya Malani on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RockPaperScissorTests/RockPaperScissorTests.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorTests.swift
+//  RockPaperScissorTests
+//
+//  Created by Chaitanya Malani on 01/04/25.
+//
+
+import Testing
+@testable import RockPaperScissor
+
+struct RockPaperScissorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
